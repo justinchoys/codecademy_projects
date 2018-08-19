4ed4fe3016e88523e19b02e8591e4b520710a942
justinchoys <justinchoys@gmail.com>
2018-08-19 19:34:01 -0400

    Include towers_of_hanoi game.

---
+++ towers_of_hanoi_game/node.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 19 19:12:04 2018
+
+@author: justin
+"""
+
+class Node:
+  def __init__(self, value, link_node=None):
+    self.value = value
+    self.link_node = link_node
+    
+  def set_next_node(self, link_node):
+    self.link_node = link_node
+    
+  def get_next_node(self):
+    return self.link_node
+  
+  def get_value(self):
+    return self.value
